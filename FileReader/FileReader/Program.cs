9c1e9096a1c9b8a62d6c19d9d74b7c011a99fd95
Steven Chen <qwe16165850@gmail.com>
2019-02-24 14:21:54 +1100

    make file path configurable

--- FileReader/FileReader/Program.cs
+++ FileReader/FileReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
 using System.Net.Sockets;
 using FileReader.Models;
@@ -10,24 +11,24 @@ namespace FileReader
         static void Main(string[] args)
         {
             //TODO: make read file configurable
-            var path = @"C:\dev\codingTest\LP_210095893_20150901T011608049.csv";
-            var fileName = Path.GetFileName(path);
+            var filePath = ConfigurationManager.AppSettings["Path"];
+            var fileName = Path.GetFileName(filePath);
             Console.WriteLine("CSV File Reader");
             if (fileName.ToLower().StartsWith("lp"))
             {
                 var lpFile = new LPFile();
-                lpFile.ReadDataFromCsvFile(path);
+                lpFile.ReadDataFromCsvFile(filePath);
             }
             else if (fileName.ToLower().StartsWith("tou"))
             {
                 var touFile = new TOUFile();
-                touFile.ReadDataFromCsvFile(path);
+                touFile.ReadDataFromCsvFile(filePath);
             }
 
             //TODO: Get Data from CSV File
 
             Console.WriteLine("Finish");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
